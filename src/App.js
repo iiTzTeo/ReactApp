f8bbcc0dd89bede6e3bd45681c1aecc23f9b324a
Vincenzo <vincenzo.cavataio@nextre.it>
2020-10-26 15:20:44 +0100

    blank

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
 import React, { useState, useEffect } from "react";
-import Post from "./InstagramPost";
 import "./App.css";
 
 function App() {
-  const [post, setPost] = useState([
-    { utente: "Matteo", descrizione: "primo giorno su react", like: 0 },
-    { utente: "Luca", descrizione: "primo giorno su react", like: 0 },
-    { utente: "Andrea", descrizione: "primo giorno su react", like: 0 },
-  ]);
-
-  console.log(post);
-  return (
-    <div className="App">
-      <Post data={post} postSetter={(a) => setPost(a)} />
-    </div>
-  );
+  return <div className="App">ciao</div>;
 }
 
 export default App;
