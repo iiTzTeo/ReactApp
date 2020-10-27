12ad9e7a0441d5d7e0e32e7c7978050b9936f08d
Vincenzo <vincenzo.cavataio@nextre.it>
2020-10-27 12:57:19 +0100

    modifiche calcolatrice vincnzo

--- src/Calcolatrice.jsx
+++ src/Calcolatrice.jsx
@@ -2,159 +2,66 @@ import React, { useState } from "react";
 
 function Calcolatrice() {
   const [contatore, setContatore] = useState(0);
+
+  const printValues = (a) => {
+    return a.map((e) => {
+      return (
+        <input
+          type="button"
+          value={e[0]}
+          onClick={(r) => console.log(r.target.value)}
+          className={e[1] ? "tastoLaterale" : e[0] === "=" ? "uguale" : "tasto"}
+        />
+      );
+    });
+  };
+
   return (
     <>
       <div className="contenuto">
         <p className="display">{contatore}</p>
       </div>
-
       <div className="contenuto">
         <div className="contenuto">
-          <input
-            type="submit"
-            id="("
-            value="("
-            onclick="n(this.id)"
-            className="tastoLaterale"
-          />
-          <input
-            type="submit"
-            id=")"
-            value=")"
-            onclick="n(this.id)"
-            className="tastoLaterale"
-          />
-          <input
-            type="submit"
-            id="%"
-            value="%"
-            onclick="n(this.id)"
-            className="tastoLaterale"
-          />
-          <input
-            type="submit"
-            id="AC"
-            value="AC"
-            onclick="n(this.id)"
-            className="tastoLaterale"
-          />
+          {printValues([
+            ["(", false],
+            [")", false],
+            ["%", false],
+            ["AC", true],
+          ])}
         </div>
 
         <div className="contenuto">
-          <input
-            type="button"
-            id="7"
-            value={7}
-            onClick={(e) => console.log(e)}
-            className="tasto"
-          />
-          <input
-            type="submit"
-            id="8"
-            value="8"
-            onclick="n(this.id)"
-            className="tasto"
-          />
-          <input
-            type="submit"
-            id="9"
-            value="9"
-            onclick="n(this.id)"
-            className="tasto"
-          />
-          <input
-            type="submit"
-            id="/"
-            value="/"
-            onclick="n(this.id)"
-            className="tastoLaterale"
-          />
+          {printValues([
+            ["7", false],
+            ["8", false],
+            ["9", false],
+            ["/", true],
+          ])}
         </div>
         <div className="contenuto">
-          <input
-            type="submit"
-            id="4"
-            value="4"
-            onclick="n(this.id)"
-            className="tasto"
-          />
-          <input
-            type="submit"
-            id="5"
-            value="5"
-            onclick="n(this.id)"
-            className="tasto"
-          />
-          <input
-            type="submit"
-            id="6"
-            value="6"
-            onclick="n(this.id)"
-            className="tasto"
-          />
-          <input
-            type="submit"
-            id="*"
-            value="x"
-            onclick="n(this.id)"
-            className="tastoLaterale"
-          />
+          {printValues([
+            ["4", false],
+            ["5", false],
+            ["6", false],
+            ["*", true],
+          ])}
         </div>
         <div className="contenuto">
-          <input
-            type="submit"
-            id="1"
-            value="1"
-            onclick="n(this.id)"
-            className="tasto"
-          />
-          <input
-            type="submit"
-            id="2"
-            value="2"
-            onclick="n(this.id)"
-            className="tasto"
-          />
-          <input
-            type="submit"
-            id="3"
-            value="3"
-            onclick="n(this.id)"
-            className="tasto"
-          />
-          <input
-            type="submit"
-            id="-"
-            value="-"
-            onclick="n(this.id)"
-            className="tastoLaterale"
-          />
+          {printValues([
+            ["1", false],
+            ["2", false],
+            ["3", false],
+            ["-", true],
+          ])}
         </div>
         <div className="contenuto">
-          <input
-            type="submit"
-            id="0"
-            value="0"
-            onclick="n(this.id)"
-            className="tasto"
-          />
-          <input
-            type="submit"
-            id="."
-            value="."
-            onclick="n(this.id)"
-            className="tasto"
-          />
-          <button value="=" onclick="operazione();" className="uguale">
-            =
-          </button>
-          <input
-            type="submit"
-            id="+"
-            value="+"
-            onclick="n(this.id)"
-            className="tastoLaterale"
-          />
+          {printValues([
+            ["0", false],
+            [".", false],
+            ["=", false],
+            ["+", true],
+          ])}
         </div>
       </div>
     </>
